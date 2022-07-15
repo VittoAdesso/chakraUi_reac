0bb5c0b0f754a018fdf992f3a541d962fe2d1503
Vittorio Adesso <adessovittorio8@gmail.com>
2022-07-16 00:25:04 +0200

    cleaning

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-   
- ReactDOM.render(
+
+ReactDOM.render(
   <React.StrictMode>
-   <ChakraProvider>
-     <ColorModeScript initialColorMode="light"></ColorModeScript>
-   <App />
-   </ChakraProvider>
+    <ChakraProvider>
+    <ColorModeScript initialColorMode="light"></ColorModeScript>
+    <App />
+    </ChakraProvider>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
